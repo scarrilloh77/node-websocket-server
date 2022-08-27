c9308d7243bdc4ae80eb0a442f4a6d261b5c03ec
Sebastian Carrillo <scarrilloh77@gmail.com>
2022-08-27 14:01:14 -0500

    Mensajes de conexión y desconexión - Cliente

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,18 @@
+// Referencias del HTML
+
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
 const socket = io(); // Socket del cliente
+
+socket.on('connect', () => {
+  console.log('conectado!');
+  lblOffline.style.display = 'none';
+  lblOnline.style.display = '';
+});
+
+socket.on('disconnect', () => {
+  console.log('Desconectado del servidor!');
+  lblOffline.style.display = '';
+  lblOnline.style.display = 'none';
+});
