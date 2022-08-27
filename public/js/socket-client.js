0aa7bf58cf43ec0bc3ad3dd543b91fbb1625ee61
Sebastian Carrillo <scarrilloh77@gmail.com>
2022-08-27 14:15:16 -0500

    Emitir desde el cliente - Escuchar en el servidor

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -3,6 +3,9 @@
 const lblOnline = document.querySelector('#lblOnline');
 const lblOffline = document.querySelector('#lblOffline');
 
+const txtMensaje = document.querySelector('#txtMensaje');
+const btnEnviar = document.querySelector('#btnEnviar');
+
 const socket = io(); // Socket del cliente
 
 socket.on('connect', () => {
@@ -15,4 +18,15 @@ socket.on('disconnect', () => {
   console.log('Desconectado del servidor!');
   lblOffline.style.display = '';
   lblOnline.style.display = 'none';
+});
+
+btnEnviar.addEventListener('click', () => {
+  const mensaje = txtMensaje.value;
+  const payload = {
+    mensaje,
+    id: '123ABC',
+    fecha: new Date().getTime(),
+  };
+
+  socket.emit('enviar-mensaje', payload);
 });
